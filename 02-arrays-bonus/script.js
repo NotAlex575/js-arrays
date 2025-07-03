bd683221bbb9a64af96a746e5d0214e9c55df804
NotAlex575 <dapontealessandro89gmail.com>
2025-07-03 20:00:24 +0200

    EXTRA: 4. Verifica se 'Fabio' è presente nell'array teachers // e salva il risultato nella variabile isFabioPresent

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,7 +25,16 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio');
+if(isFabioPresent >= 0){
+  console.log("Fabio è nella posizione: "+isFabioPresent);
+}
+else
+{
+  console.log("Fabio non esiste nell'array....");
+}
+
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
